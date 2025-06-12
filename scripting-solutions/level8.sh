e9698f9e4e469cff247d31a0eb2fed0aeb071a1f
SaidInTheCloud <76113721+SaidInTheCloud@users.noreply.github.com>
2025-06-13 00:14:54 +0100

    Level 8 Updated

--- scripting-solutions/level8.sh
+++ scripting-solutions/level8.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+Directory="." # specify what directory you are looking for
+search_term="random22" # specify the search term
+
+if [ ! -d "$Directory" ]; then
+    echo "Directory doesnt exist"
+    exit 1
+fi
+
+grep -l "$search_term" "$Directory"/*.log 
